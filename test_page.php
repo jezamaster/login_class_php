507acfd6880681c45d371397c6a36ad4490944e0
jezamaster <47149858+jezamaster@users.noreply.github.com>
2021-05-19 22:19:15 +0200

    Create test_page.php

---
+++ test_page.php
@@ -0,0 +1,15 @@
+<?php session_start();
+ require_once("sqlPDO.php");
+ require_once("login.php");
+?>
+<!doctype html>
+<html lang="en">
+<nav>
+  <a href="contact.php">Contact </a>
+  <?php include "login_link.php"; ?>
+</nav>
+<body>
+  
+ 
+ </body>  
+</html>
